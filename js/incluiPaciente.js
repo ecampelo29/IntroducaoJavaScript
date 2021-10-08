c4bcb4bd3b7fd6601d02bbec05ff81c746d2a071
unknown <EmersonCampelo@TROIA.icarotech.com>
2021-10-08 08:42:15 -0300

    aula 5 refatorando com inclusão de classes nas estruturas criadas

--- js/incluiPaciente.js
+++ js/incluiPaciente.js
@@ -3,6 +3,52 @@
 // identificando o botão
 const botao_adicionar = document.querySelector('#adicionar-paciente')
 
+// função que busca dados do paciente
+const obtemPacienteDoFormulario = (formulario) => {
+    // adicionando informações usando a propriedade value do formulário
+    // objeto paciente
+    const paciente = {
+        nome: formulario.nome.value,
+        peso: formulario.peso.value,
+        altura: formulario.altura.value,
+        gordura: formulario.gordura.value
+    }
+    return paciente;
+}
+
+const montaTd = (info, classe) => {
+    
+    const td  = document.createElement('td');
+    td.textContent = info;
+    td.classList.add(classe);
+
+    return td
+}
+
+// função que monta a tabela
+const montaTr = (paciente) => {
+     // criando uma estrutua de tr (linha da tabela)
+     const pacienteTr = document.createElement('tr');
+     pacienteTr.classList.add("paciente")
+     const nomeTd = montaTd (paciente.nome, "info-nome")
+     const pesoTd =  montaTd (paciente.peso, "info-peso")
+     const alturaTd =  montaTd (paciente.altura, "info-altura")
+     const gorduraTd =  montaTd (paciente.gordura, "info-gordura")
+     const imcTd =  document.createElement('td')
+
+     calculoImc(pesoTd.textContent, alturaTd.textContent, imcTd)
+ 
+     // montando a estrutura no html
+     pacienteTr.appendChild(nomeTd);
+     pacienteTr.appendChild(pesoTd);
+     pacienteTr.appendChild(alturaTd);
+     pacienteTr.appendChild(gorduraTd);
+     pacienteTr.appendChild(imcTd);
+
+     return pacienteTr;
+
+}
+
 // listener com função anônima
 botao_adicionar.addEventListener('click', (event)=>{
     event.preventDefault();
@@ -11,28 +57,11 @@ botao_adicionar.addEventListener('click', (event)=>{
     const formulario = document.querySelector('#form-adiciona')
     // capturando a tabela
     const tabela = document.querySelector('#tabela-pacientes')
-
-    // criando uma estrutua de tr (linha da tabela)
-    const pacienteTr = document.createElement('tr');
-    const nomeTd = document.createElement('td');
-    const pesoTd = document.createElement('td');
-    const alturaTd = document.createElement('td');
-    const gorduraTd = document.createElement('td');
-    const imcTd = document.createElement('td');
-    
-    // adicionando informações usando a propriedade value do formulário
-    nomeTd.textContent = formulario.nome.value;
-    pesoTd.textContent = formulario.peso.value;
-    alturaTd.textContent = formulario.altura.value;
-    gorduraTd.textContent = formulario.gordura.value;
-    //calculoImc(pesoTd.textContent, alturaTd.textContent, imcTd)
-
-    // montando a estrutura no html
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    // criando o paciente
+    const paciente = obtemPacienteDoFormulario(formulario)
+    // criando o Tr
+    const pacienteTr = montaTr(paciente)
+    // incluindo o Tr na tabela
     tabela.appendChild(pacienteTr)
 
 
