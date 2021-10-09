e979a046ea57afcbee775fcc956f0ebdcc545ee7
Emerson Campelo <ecampelo29@gmail.com>
2021-10-09 13:50:17 -0300

    fim da aula 8 - implementando filtragem na tabela de pacientes

---
+++ js/filtro-tabela.js
@@ -0,0 +1,23 @@
+const campoFiltro = document.querySelector("#filtrar-tabela")
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    const pacientes = document.querySelectorAll(".paciente")
+
+    if (this.value.length > 0) {
+        pacientes.forEach((paciente) =>{
+            const nome = paciente.querySelector(".info-nome").textContent
+            const regex = new RegExp(this.value, "i") //modificador para desconsiderar maíusculo
+            if (regex.test(nome)) {
+                paciente.classList.remove("invisivel")
+            } else {
+                paciente.classList.add("invisivel")
+            }
+        })
+    } else {
+        pacientes.forEach((paciente) =>{
+            paciente.classList.remove("invisivel")
+        })
+    }
+
+})
